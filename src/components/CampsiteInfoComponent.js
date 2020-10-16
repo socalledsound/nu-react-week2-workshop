fbbd884d9ac100802939d05f6abf527271fd973c
Chris Kubick <socalledsound@gmail.com>
2020-10-16 12:08:38 -0700

    task 2 complete

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 class CampsiteInfoComponent extends Component {
+
+
+
+    renderCampsite(campsite){
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+
+    }
+
 
     render(){
         if(this.props.campsite){
             return (
                 <div className="row">
-                    hi
+                    {this.renderCampsite(this.props.campsite)}
+
                 </div>
             )
         }
         return (
-            <div>
-                error passing campsite
-            </div>
+            <div />
         )
     }
 
