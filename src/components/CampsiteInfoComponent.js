6cb75f73219ab1053bf12f3e32b677c86a1bea60
Chris Kubick <socalledsound@gmail.com>
2020-10-16 12:17:08 -0700

    task3 completed

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -20,11 +20,37 @@ class CampsiteInfoComponent extends Component {
     }
 
 
+    renderComments(comments){
+        if(comments){
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>comments</h4>
+                    {
+                        comments.map( comment => {
+                            return (
+                                <div key={comment.id}>
+                                <p>
+                                    {comment.text}<br/>
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                            </div>    
+                            )
+
+
+                        })
+                    }
+                </div>
+            )
+        }
+    }
+
+
     render(){
         if(this.props.campsite){
             return (
                 <div className="row">
                     {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
 
                 </div>
             )
